d72421d8f5c65b171459d87d98c262182441b6de
Chris Jaehnen <cjaehnen@me.com>
2018-06-10 21:52:56 -0400

    Update shared pipeline for Maven Central publishing

--- tasks/publish.sh
+++ tasks/publish.sh
@@ -6,6 +6,14 @@ source ${work_dir}/shared/commons.sh
 
 cd source
 
+echo ${artifact_repo_signing_key_secret_keys} >> secret-keys.gpg
+
+echo "nexusUsername=${artifact_repo_username}" >> gradle.properties
+echo "nexusPassword=${artifact_repo_password}" >> gradle.properties
+echo "signing.keyId=${artifact_repo_signing_key_id}" >> gradle.properties
+echo "signing.password=${artifact_repo_signing_key_passphrase}" >> gradle.properties
+echo "signing.secretKeyRingFile=secret-keys.gpg" >> gradle.properties
+
 if [ "${env}" == "release" ] ; then
     ./gradlew clean release -Prelease.useAutomaticVersion=true -x test
 else
--- tasks/shared/commons.sh
+++ tasks/shared/commons.sh
@@ -2,7 +2,7 @@
 set -e -x -u
 
 function get_artifact_file {
-    artifact_id=$1
+    local artifact_id="$1"
     artifact_file=`find $(pwd) -name ${artifact_id}*jar`
     echo ${artifact_file}
 }
@@ -43,15 +43,15 @@ function get_version {
 }
 
 function get_version_from_artifact_file {
-    artifact_id=$1
+    local artifact_id="$1"
     version=`ls *.jar | sed "s/${artifact_id}-//g" | sed s/.jar//g`
     echo ${version}
 }
 
 function pcf_create_cups {
-    pcf_app_name=$1
-    pcf_service_name=$2
-    pcf_service_uri=$3
+    local pcf_app_name="$1"
+    local pcf_service_name="$2"
+    local pcf_service_uri="$3"
     cf us ${pcf_app_name} ${pcf_service_name} || { echo "CUPS ${pcf_service_name} not found. Cannot unbind. Continuing on..." >&2; }
     cf ds ${pcf_service_name} -f || { echo "CUPS ${pcf_service_name} not found. Cannot delete. Continuing on..." >&2; }
     cf cups ${pcf_service_name} -p "{\"uri\":\"${pcf_service_uri}\"}"
@@ -59,10 +59,10 @@ function pcf_create_cups {
 }
 
 function pcf_create_service {
-    pcf_app_name=$1
-    pcf_service_name=$2
-    pcf_service_type=$3
-    pcf_service_plan=$4
+    local pcf_app_name="$1"
+    local pcf_service_name="$2"
+    local pcf_service_type="$3"
+    local pcf_service_plan="$4"
     cf us ${pcf_app_name} ${pcf_service_name} || { echo "Service ${pcf_service_name} not found. Cannot unbind. Continuing on..." >&2; }
     cf ds ${pcf_service_name} -f || { echo "Service ${pcf_service_name} not found. Cannot delete. Continuing on..." >&2; }
     cf cs ${pcf_service_type} ${pcf_service_plan} ${pcf_service_name}
@@ -70,7 +70,7 @@ function pcf_create_service {
 }
 
 function pcf_create_service_registry {
-    pcf_service_registry_name=$1
+    local pcf_service_registry_name="$1"
     pcf_service_type=p-service-registry standard
     pcf_service_plan=standard
     cf service ${pcf_service_registry_name} || { \
@@ -82,22 +82,22 @@ function pcf_create_service_registry {
 }
 
 function pcf_login {
-    pcf_api_endpoint=$1
-    pcf_org_name=$2
-    pcf_space_name=$3
-    pcf_username=$4
-    pcf_password=$5
+    local pcf_api_endpoint="$1"
+    local pcf_org_name="$2"
+    local pcf_space_name="$3"
+    local pcf_username="$4"
+    local pcf_password="$5"
     cf login -a ${pcf_api_endpoint} -o ${pcf_org_name} -s ${pcf_space_name} -u ${pcf_username} -p ${pcf_password} --skip-ssl-validation
 }
 
 function pcf_push {
-    pcf_app_name=$1
+    local pcf_app_name="$1"
     cf push
 }
 
 function set_manifest_properties {
-    artifact_id=$1
-    pcf_app_name=$2
+    local artifact_id="$1"
+    local pcf_app_name="$2"
     sed -e "s/path\:.*/path\: ${artifact_id}\.jar/g" manifest.yml -i
     sed -e "s/name\:\s${artifact_id}.*/name\: ${pcf_app_name}/g" manifest.yml -i
     echo "Manifest properties set successfully!" >&2
