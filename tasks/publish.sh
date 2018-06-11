f693424e328efe9a1f0fb7159604a18b2a32dcd3
Chris Jaehnen <cjaehnen@me.com>
2018-06-10 21:59:51 -0400

    Update shared pipeline for Maven Central publishing

--- tasks/publish.sh
+++ tasks/publish.sh
@@ -15,29 +15,7 @@ echo "signing.password=${artifact_repo_signing_key_passphrase}" >> gradle.proper
 echo "signing.secretKeyRingFile=secret-keys.gpg" >> gradle.properties
 
 if [ "${env}" == "release" ] ; then
-    ./gradlew clean release -Prelease.useAutomaticVersion=true -x test
+    ./gradlew clean release -Prelease.useAutomaticVersion=true uploadArchives -x test
 else
-    ./gradlew clean assemble
+    ./gradlew clean assemble uploadArchives
 fi
-
-group_id=$(get_group_id)
-group_id_path=$(get_group_id_path)
-artifact_id=$(get_artifact_id)
-
-cd build/libs
-
-version=$(get_version_from_artifact_file ${artifact_id})
-packaging=jar
-generate_pom=true
-artifact_file=$(get_artifact_file ${artifact_id})
-
-options="-Durl=${artifact_repo_uri} "
-options="${options} -DrepositoryId=${artifact_repo_id} "
-options="${options} -DgroupId=${group_id} "
-options="${options} -DartifactId=${artifact_id} "
-options="${options} -Dversion=${version} "
-options="${options} -Dpackaging=${packaging} "
-options="${options} -DgeneratePom=${generate_pom} "
-options="${options} -Dfile=${artifact_file}"
-
-mvn deploy:deploy-file ${options}
