d7a448cdc011ca277d201406524f107e2925c8e5
Chris Jaehnen <cjaehnen@me.com>
2018-06-12 13:47:24 -0400

    Add optional project directory for publish

--- tasks/publish.sh
+++ tasks/publish.sh
@@ -1,21 +1,27 @@
 #!/bin/bash
 set -e -x -u
 
-work_dir=$(dirname $0)
-source ${work_dir}/shared/commons.sh
-
 cd source
 
-echo ${artifact_repo_signing_key_secret_keys} >> secret-keys.gpg
+if [ "${env}" == "release" ] ; then
+    ./gradlew clean release -Prelease.useAutomaticVersion=true -x test
+else
+    ./gradlew clean assemble
+fi
 
-echo "nexusUsername=${artifact_repo_username}" >> gradle.properties
+if [ -d "${project_dir}" ]; then
+    cd ${project_dir}
+fi
+
+echo ${artifact_repo_signing_key_secret_keys} > secret-keys.gpg
+
+echo "nexusUsername=${artifact_repo_username}" > gradle.properties
 echo "nexusPassword=${artifact_repo_password}" >> gradle.properties
 echo "signing.keyId=${artifact_repo_signing_key_id}" >> gradle.properties
 echo "signing.password=${artifact_repo_signing_key_passphrase}" >> gradle.properties
 echo "signing.secretKeyRingFile=secret-keys.gpg" >> gradle.properties
 
-if [ "${env}" == "release" ] ; then
-    ./gradlew clean release -Prelease.useAutomaticVersion=true uploadArchives -x test
-else
-    ./gradlew clean assemble uploadArchives
-fi
+.gradlew uploadArchives
+
+rm -f gradle.properties
+rm -f secret-keys.gpg
