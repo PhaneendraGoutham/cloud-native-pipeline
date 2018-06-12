19b397c84b902a0cf7b94cc76709af313b2591b2
Chris Jaehnen <cjaehnen@me.com>
2018-06-12 15:49:14 -0400

    Add relative path reference for project dir

--- tasks/publish.sh
+++ tasks/publish.sh
@@ -11,18 +11,25 @@ fi
 
 if [ -d "${project_dir}" ]; then
     cd ${project_dir}
-fi
 
-echo ${artifact_repo_signing_key_secret_keys} > secret-keys.gpg
+    echo ${artifact_repo_signing_key_secret_keys} > secret-keys.gpg
 
-echo "nexusUsername=${artifact_repo_username}" > gradle.properties
-echo "nexusPassword=${artifact_repo_password}" >> gradle.properties
-echo "signing.keyId=${artifact_repo_signing_key_id}" >> gradle.properties
-echo "signing.password=${artifact_repo_signing_key_passphrase}" >> gradle.properties
-echo "signing.secretKeyRingFile=secret-keys.gpg" >> gradle.properties
+    echo "nexusUsername=${artifact_repo_username}" > gradle.properties
+    echo "nexusPassword=${artifact_repo_password}" >> gradle.properties
+    echo "signing.keyId=${artifact_repo_signing_key_id}" >> gradle.properties
+    echo "signing.password=${artifact_repo_signing_key_passphrase}" >> gradle.properties
+    echo "signing.secretKeyRingFile=secret-keys.gpg" >> gradle.properties
 
-if [ -d "${project_dir}" ]; then
     ${get_cd_up_path ${project_dir}}gradlew uploadArchives
 else
+    echo ${artifact_repo_signing_key_secret_keys} > secret-keys.gpg
+
+    echo "" >> gradle.properties
+    echo "nexusUsername=${artifact_repo_username}" >> gradle.properties
+    echo "nexusPassword=${artifact_repo_password}" >> gradle.properties
+    echo "signing.keyId=${artifact_repo_signing_key_id}" >> gradle.properties
+    echo "signing.password=${artifact_repo_signing_key_passphrase}" >> gradle.properties
+    echo "signing.secretKeyRingFile=secret-keys.gpg" >> gradle.properties
+
     ./gradlew uploadArchives
 fi
